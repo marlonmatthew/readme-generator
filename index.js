60d3da79a4766ef1ea54bebd21090b3519040eb3
Marlon Matthew <mbm.drums@gmail.com>
2021-04-14 11:40:39 -0700

    Added the array of questions for the user to enter information and supply the content for the readme.

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of the project: ",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of the project: ",
+  },
+  {
+    type: "input",
+    name: "installationInstructions",
+    message: "Enter the installation instructions: ",
+  },
+  {
+    type: "input",
+    name: "usageInfo",
+    message: "Enter the usage information: ",
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "Enter the contribution guidelines: ",
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "Enter the test instructions: ",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Select the license for the project: ",
+    choices: ["APACHE", "BSD", "GPL", "MIT", "MPL", "CDDL", "EPL", "None"],
+  },
+  {
+    type: "input",
+    name: "githubName",
+    message: "Enter your GitHub username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address: ",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
