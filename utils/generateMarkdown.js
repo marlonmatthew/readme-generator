a79bb7809694a33af40533488d1ab5e12dc040cf
Marlon Matthew <mbm.drums@gmail.com>
2021-04-14 12:36:02 -0700

    Content added to generateMarkdown.js including title, table of contents, description, installation, usage, license, contributing, tests, and questions.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,68 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `Title: ${data.title}
+
+License: ${data.license}
+
+## Table of Contents
+
+* [Description](#description)
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+-------------------------------
+
+## Description
+
+${data.description}
+
+-------------------------------
+
+## Installation
+
+${data.installationInstructions}
+
+-------------------------------
+
+## Usage
+
+${data.usageInfo}
+
+-------------------------------
+
+## License
+
+This application is covered under the ${data.license} license.
+
+-------------------------------
+  
+## Contributing
+
+${data.contributionGuidelines}
+
+-------------------------------
+
+## Tests
+
+${data.testInstructions}
+
+-------------------------------
+
+## Questions
+
+My email: ${data.email}
+
+My GitHub: https://github.com/${data.githubName}/
 
 `;
 }
